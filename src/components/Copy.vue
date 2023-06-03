366b95b253b0c9839a8596f9787cf484a441851b
Artem Litvinko <artem.litvinko@e-sphere.ch>
2023-06-03 14:59:57 +0200

    customize app

--- src/components/Copy.vue
+++ src/components/Copy.vue
@@ -5,76 +5,76 @@ import { ref } from "vue";
 
 const porps = defineProps<{ content: string }>();
 const btnConfig: {
-  size: number;
-  fill: string;
-  theme: Theme;
+    size: number;
+    fill: string;
+    theme: Theme;
 } = {
-  size: 14,
-  fill: "#999",
-  theme: "outline",
+    size: 14,
+    fill: "#999",
+    theme: "outline",
 };
 const btnTips = {
-  copy: "复制全文",
-  loading: "",
-  success: "已复制到剪贴板！",
-  error: "复制失败！",
+    copy: "复制全文",
+    loading: "",
+    success: "Success!",
+    error: "Error!",
 };
 const btnStatus = ref<"copy" | "loading" | "success" | "error">("copy");
 
 const copyToClipboard = (content: string = porps.content) => {
-  btnStatus.value = "loading";
-  navigator.clipboard
-    .writeText(content)
-    .then(() => setTimeout(() => (btnStatus.value = "success"), 150))
-    .catch(() => (btnStatus.value = "error"))
-    .finally(() => setTimeout(() => (btnStatus.value = "copy"), 1500));
+    btnStatus.value = "loading";
+    navigator.clipboard
+        .writeText(content)
+        .then(() => setTimeout(() => (btnStatus.value = "success"), 150))
+        .catch(() => (btnStatus.value = "error"))
+        .finally(() => setTimeout(() => (btnStatus.value = "copy"), 1500));
 };
 </script>
 
 <template>
-  <div class="flex items-center cursor-pointer" @click="copyToClipboard()">
-    <copy
-      v-show="btnStatus === 'copy'"
-      :theme="btnConfig.theme"
-      :size="btnConfig.size"
-      :fill="btnConfig.fill"
-    />
-    <loading
-      class="rotate"
-      v-show="btnStatus === 'loading'"
-      :theme="btnConfig.theme"
-      :size="btnConfig.size"
-      :fill="btnConfig.fill"
-    />
-    <check-one
-      v-show="btnStatus === 'success'"
-      :theme="btnConfig.theme"
-      :size="btnConfig.size"
-      :fill="btnConfig.fill"
-    />
-    <close-one
-      v-show="btnStatus === 'error'"
-      :theme="btnConfig.theme"
-      :size="btnConfig.size"
-      :fill="btnConfig.fill"
-    />
-    <span class="text-xs ml-0.5 text-gray-500 leading-none">{{
-      btnTips[btnStatus]
-    }}</span>
-  </div>
+    <div class="flex items-center cursor-pointer" @click="copyToClipboard()">
+        <copy
+            v-show="btnStatus === 'copy'"
+            :theme="btnConfig.theme"
+            :size="btnConfig.size"
+            :fill="btnConfig.fill"
+        />
+        <loading
+            class="rotate"
+            v-show="btnStatus === 'loading'"
+            :theme="btnConfig.theme"
+            :size="btnConfig.size"
+            :fill="btnConfig.fill"
+        />
+        <check-one
+            v-show="btnStatus === 'success'"
+            :theme="btnConfig.theme"
+            :size="btnConfig.size"
+            :fill="btnConfig.fill"
+        />
+        <close-one
+            v-show="btnStatus === 'error'"
+            :theme="btnConfig.theme"
+            :size="btnConfig.size"
+            :fill="btnConfig.fill"
+        />
+        <span class="text-xs ml-0.5 text-gray-500 leading-none">{{
+            btnTips[btnStatus]
+        }}</span>
+    </div>
 </template>
 
 <style scoped>
 @keyframes spin {
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(360deg);
-  }
+    0% {
+        transform: rotate(0deg);
+    }
+    100% {
+        transform: rotate(360deg);
+    }
 }
 
 .rotate {
-  animation: spin 2s linear infinite;
+    animation: spin 2s linear infinite;
 }
 </style>
--- src/libs/markdown.ts
+++ src/libs/markdown.ts
@@ -2,23 +2,22 @@ import Markdown from "markdown-it";
 import highlight from "highlight.js";
 
 const mdOptions: Markdown.Options = {
-  linkify: true,
-  typographer: true,
-  breaks: true,
-  langPrefix: "language-",
-  // 代码高亮
-  highlight(str, lang) {
-    if (lang && highlight.getLanguage(lang)) {
-      try {
-        return (
-          '<pre class="hljs"><code>' +
-          highlight.highlight(lang, str, true).value +
-          "</code></pre>"
-        );
-      } catch (__) {}
-    }
-    return "";
-  },
+    linkify: true,
+    typographer: true,
+    breaks: true,
+    langPrefix: "language-",
+    highlight(str, lang) {
+        if (lang && highlight.getLanguage(lang)) {
+            try {
+                return (
+                    '<pre class="hljs"><code>' +
+                    highlight.highlight(lang, str, true).value +
+                    "</code></pre>"
+                );
+            } catch (__) {}
+        }
+        return "";
+    },
 };
 
 export const md = new Markdown(mdOptions);
